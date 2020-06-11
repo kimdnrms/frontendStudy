fe6f312f5353cdd84f16dba7309238df8af1f880
WookeunKim <kimdnrms@gmail.com>
2020-06-11 21:53:19 +0900

    commit google most visited function 만드는중 - img > CSS로 대체. template 골격 완성

--- Step_1_CloneCoding/1st/googlePracticeJs.js
+++ Step_1_CloneCoding/1st/googlePracticeJs.js
@@ -1,15 +1,17 @@
+let divCount = 0;
+
 const inactive = () => {
-    var obj = document.getElementById("searchContents");
+    var obj = document.getElementById('searchContents');
 }
 
 const mouseover_topContainer_item_app = () => {
-    var tmp = document.getElementById("topContainer_item_img");
-    tmp.src = "./img/headMenu_app_over.png";
+    var tmp = document.getElementById('topContainer_item_img');
+    tmp.src = './img/headMenu_app_over.png';
 }
 
 const mouseout_topContainer_item_app = () => {
-    var tmp = document.getElementById("topContainer_item_img");
-    tmp.src = "./img/headMenu_app.png";
+    var tmp = document.getElementById('topContainer_item_img');
+    tmp.src = './img/headMenu_app.png';
 }
 
 const showModalTapApp = () => {
@@ -26,11 +28,13 @@ const onFocusSearchInputBox = () => {
 
 
 const mouseover_mostVisitedContainer_item_app = () => {
-    document.getElementsByClassName("mostVisitedItem").style.backgroundColor = "rgba(238,238,238,1)";
+    document.querySelector('.mostVisitedItem').style.backgroundColor = 'rgba(238,238,238,1)';    
+    document.getElementById('addMostVistedButtonIcon').style.backgroundColor = 'rgba(220,220,220,1)';
 }
 
 const mouseout_mostVisitedContainer_item_app = () => {
-    document.getElementsByClassName("mostVisitedItem").style.backgroundColor = "rgba(255,255,255)";
+    document.querySelector('.mostVisitedItem').style.backgroundColor = 'rgba(255,255,255)';
+    document.getElementById('addMostVistedButtonIcon').style.backgroundColor = 'rgba(238,238,238,1)';
 
 }
 
@@ -38,10 +42,26 @@ const showModalAddMostVisited = () => {
 
 }
 
-const addButton = () => {
-    let areaDiv = document.getElementById("mostVisitedItemContainer");
+const addOKButton = () => {
+    if(divCount < 8) {
+        var form_container = document.createElement('div');
+        form_container.className = 'mostVisitedItem';
+        form_container.id = 'mostVisitedItem_' + divCount;        
+       
+        const template = document.querySelector('.newAddedMostVistedTemplate');
+        const clone = document.importNode(template.content, true);
+        
+        const areaDiv = document.getElementById('addedMostVisited');
+
+        form_container.appendChild(clone);
+        areaDiv.appendChild(form_container);
+
+        divCount ++;
+    }
+    // console.log('....')
+    // let areaDiv = document.getElementById('mostVisitedItemContainer');
     
-    const template = document.querySelector('.newDIVtemplate');
-    const clone = document.importNode(template.content, true);
-    areaDiv.appendChild(clone);
+    // const template = document.querySelector('.newDIVtemplate');
+    // const clone = document.importNode(template.content, true);
+    // areaDiv.appendChild(clone);
 }
